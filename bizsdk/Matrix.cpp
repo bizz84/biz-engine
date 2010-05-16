95ed40e439a28e28d6f79312d3b4d6720d8e375a
Andrea Bizzotto <bizz84@gmail.com>
2010-05-16 10:46:21 +0100

    - Moved Vector declarations to Vector.h/cpp - Added const qualifier where possible

--- bizsdk/Matrix.cpp
+++ bizsdk/Matrix.cpp
@@ -17,101 +17,6 @@
 #include <stdio.h>
 
 /*****************************************************************************
- * Vector2 implementation
- *****************************************************************************/
-Vector2::Vector2()
-{
-	s[0] = s[1] = 0.0f;
-}
-
-Vector2::Vector2(const float *f)
-{
-	s[0] = f[0];
-	s[1] = f[1];
-}
-Vector2::Vector2(const float x, const float y)
-{
-	s[0] = x;
-	s[1] = y;
-}
-
-/*****************************************************************************
- * Vector3 implementation
- *****************************************************************************/
-Vector3::Vector3()
-{
-	s[0] = s[1] = s[2] = 0.0f;
-}
-
-Vector3::Vector3(const float *f)
-{
-	s[0] = f[0];
-	s[1] = f[1];
-	s[2] = f[2];
-}
-
-Vector3::Vector3(const float x, const float y, const float z)
-{
-	s[0] = x;
-	s[1] = y;
-	s[2] = z;
-}
-
-ostream &operator<<(ostream &stream, const Vector3 &v)
-{
-	stream
-	  << v.s[0] << ' ' << v.s[1] << ' ' << v.s[2] << endl;
-	return stream;
-}
-
-
-/*****************************************************************************
- * Vector4 implementation
- *****************************************************************************/
-
-Vector4::Vector4()
-{
-	s[0] = s[1] = s[2] = s[3] = 0.0f;
-}
-
-Vector4::Vector4(const float *f)
-{
-	float *p = s;
-	*p++ = *f++;
-	*p++ = *f++;
-	*p++ = *f++;
-	*p++ = *f++;
-}
-Vector4::Vector4(const float x, const float y, const float z, const float w)
-{
-	float *p = s;
-	*p++ = x;
-	*p++ = y;
-	*p++ = z;
-	*p++ = w;
-
-}
-
-float Vector4::dot(const Vector4 &v)
-{
-	return s[0]*v.s[0] + s[1]*v.s[1] + s[2]*v.s[2] + s[3]*v.s[3];
-}
-
-float Vector4::dot(const float *f)
-{
-	return s[0]*f[0] + s[1]*f[1] + s[2]*f[2] + s[3]*f[3];
-}
-
-void Vector4::AssignTo(float *f)
-{
-	float *p = s;
-	*f++ = *p++;
-	*f++ = *p++;
-	*f++ = *p++;
-	*f++ = *p++;
-}
-
-/*****************************************************************************
  * Matrix3 implementation
  *****************************************************************************/
 
@@ -128,7 +33,7 @@ Matrix3::Matrix3(const float *f)
 		*p++ = *f++;
 }
 
-Matrix3 Matrix3::RotationX(const float angle)
+const Matrix3 Matrix3::RotationX(const float angle)
 {
 	float c = cos(angle);
 	float s = sin(angle);
@@ -140,7 +45,7 @@ Matrix3 Matrix3::RotationX(const float angle)
 	return Matrix3(f);
 }
 
-Matrix3 Matrix3::RotationY(const float angle)
+const Matrix3 Matrix3::RotationY(const float angle)
 {
 	float c = cos(angle);
 	float s = sin(angle);
@@ -152,7 +57,7 @@ Matrix3 Matrix3::RotationY(const float angle)
 	return Matrix3(f);
 }
 
-Matrix3 Matrix3::RotationZ(const float angle)
+const Matrix3 Matrix3::RotationZ(const float angle)
 {
 	float c = cos(angle);
 	float s = sin(angle);
@@ -164,7 +69,7 @@ Matrix3 Matrix3::RotationZ(const float angle)
 	return Matrix3(f);
 }
 
-Vector3 Matrix3::operator*(const Vector3 &v)
+const Vector3 Matrix3::operator*(const Vector3 &v) const
 {
 	return Vector3(
 		Vector3(s+0).dot(v),
@@ -172,7 +77,7 @@ Vector3 Matrix3::operator*(const Vector3 &v)
 		Vector3(s+6).dot(v));
 }
 
-Matrix3 Matrix3::operator*(const Matrix3 &m)
+const Matrix3 Matrix3::operator*(const Matrix3 &m) const
 {
 	Vector3 x(s+0), y(s+3), z(s+6);
 	Vector3 mx(m.s[0], m.s[3], m.s[6]);
