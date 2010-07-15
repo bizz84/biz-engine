030e7409e2d103e4da5350d3ee057a4a889af7a0
Andrea Bizzotto <bizz84@gmail.com>
2010-07-15 21:46:06 +0100

    - Fixed projection matrices - Cleaned up code

--- demos/big-head-screamers/BigHeadScreamers.cpp
+++ demos/big-head-screamers/BigHeadScreamers.cpp
@@ -7,7 +7,7 @@
  *
  * Platform			LinuxX11 / OpenGL
  * 
- * Description		Lighting & BigHeadScreamers demo using stencil buffers
+ * Description		BigHeadScreamers
  *
  *****************************************************************************/
 
@@ -17,38 +17,26 @@
 #include <iostream>
 
 
-const float TetraVertices[20] = {
+static const float TetraVertices[20] = {
 	 0.0f    ,  1.0f    ,  0.0     , 0.5f, 1.0f,
 	 0.0f    , -0.33333f,  0.94281f, 0.5f, 0.0f, 
 	-0.81650f, -0.33333f, -0.47140f, 0.0f, 0.0f,
 	 0.81650f, -0.33333f, -0.47140f, 1.0f, 0.0f,
 };
 
-const int TetraIndices[12] = {
+static const int TetraIndices[12] = {
 	0, 1, 2,
 	0, 2, 3,
 	0, 3, 1,
 	1, 2, 3
 };
 
-const float Colors[] = {
 
-	0.0f, 1.0f, 0.0f, 0.8f,
-	0.0f, 1.0f, 0.0f, 0.6f,
-	0.0f, 1.0f, 0.0f, 0.4f,
-	0.0f, 1.0f, 0.0f, 0.2f,
-	1.0f, 1.0f, 0.0f, 0.2f,
-};
+static const float Near = 1.0f;
+static const float Far = 100000.0f;
+static const float Height = 20.0f;
 
-const float GrayColor[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-const float WhiteColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
-
-const float Near = 10.0f;
-const float Far = 40000.0f;
-
-
-
-const char *szSkybox[] = {
+static const char *szSkybox[] = {
 	"data/textures/alpine_east.bmp",
 	"data/textures/alpine_west.bmp",
 	"data/textures/alpine_up.bmp",
@@ -58,45 +46,25 @@ const char *szSkybox[] = {
 };
 
 
+static const char *shaders[] = {
+	"data/shaders/Lookup.vert", "data/shaders/Lookup.frag",
+	"data/shaders/LookupColor.vert", "data/shaders/LookupColor.frag",
+	"data/shaders/ColorOffset.vert", "data/shaders/ColorOffset.frag",
+	"data/shaders/UniformColor.vert", "data/shaders/UniformColor.frag",
+};
+
+
 BigHeadScreamers::BigHeadScreamers()
-	: launcher(0.1f)
+	: launcher(0.1f),
+	iShowInfo(0),
+	uiBGTexture(~0),
+	pTetraVBO(NULL)
 {
-	bWireframe = false;
-
-	pTetraVBO = NULL;
-	pRoomMesh = NULL;
-
-	iCurLight = 0;
-	iNumLights = 1;
-	light[0].fLightSpeed = 0.5f;
-	light[0].fLightAngle = 0.0f;
-	light[0].fLightHeight = 100.0f;
-	light[0].fLightRadius = 1.0f;
-
-	light[1].fLightSpeed = 0.6f;
-	light[1].fLightAngle = 0.5 * M_PI;
-	light[1].fLightHeight = -100.0f;
-	light[1].fLightRadius = 1.2f;
-
-	light[2].fLightSpeed = 0.35f;
-	light[2].fLightAngle = M_PI;
-	light[2].fLightHeight = 50.0f;
-	light[2].fLightRadius = 1.5f;
-
-	light[3].fLightSpeed = 0.4f;
-	light[3].fLightAngle = M_PI + M_PI_2;
-	light[3].fLightHeight = 40.0f;
-	light[3].fLightRadius = 1.8f;
-
-
-	bLightMove = true;
 
 }
 
 bool BigHeadScreamers::InitApp()
 {
-	iShowInfo = 0;
-
 	// Custom command line processing
 	vector<CmdLineParameter>::iterator iter;
 	vector<CmdLineParameter> &params = GetCommandLineParams();
@@ -109,7 +77,7 @@ bool BigHeadScreamers::InitApp()
 		}
 	}	
 
-	UnitTestRun();
+	//UnitTestRun();
 
 	return true;
 }
@@ -130,6 +98,9 @@ bool BigHeadScreamers::InitGL()
 {
 	GLResourceManager &loader = GLResourceManager::Instance();
 
+	if (!ground.Init())
+		return false;
+
 	// Load Shaders
 	if (!LoadShaders())
 		return false;
@@ -142,17 +113,7 @@ bool BigHeadScreamers::InitGL()
 		uiBGTexture, GL_LINEAR, GL_LINEAR))
 		return false;
 	
-	unsigned int handle3DSFile;
-	if (!loader.Load3DSFile("data/scenes/primitives.3ds", handle3DSFile))
-		return false;
-
-
-	// Load outer room
-	pRoomMesh = new Mesh(loader.FindMesh(handle3DSFile, "Box"));
-	
-
-	CalculateBoundaries(handle3DSFile);
-
+	CoordinateFrame::Instance().Make(400);
 
 	pTetraVBO = new IndexedVBO((void *)TetraVertices, sizeof(float) * 5, 4,
 	                           (void *)TetraIndices, 12);
@@ -181,35 +142,12 @@ bool BigHeadScreamers::InitGL()
 
 	glDisable(GL_DEPTH_TEST);
 
-	// Hack
-	fMaxDistance *= 3.0f;
 
-	fpsCamera.Init(fDefDistance, -20.0f);
+	fpsCamera.Init(5.0f * Height, -Height);
+	//fpsCamera.Init(0.1 * Far, -Height);
 
 	timer.Start();
 	return true;
-}
-
-void BigHeadScreamers::CalculateBoundaries(int index3ds)
-{
-	GLResourceManager &loader = GLResourceManager::Instance();
-
-	// Calculating maximum and minimum range for zoom
-	fBoxSize = loader.MeshSize(index3ds, "Box");
-
-	fMaxDistance = 0.5f * fBoxSize;
-	
-	float d = fMinDistance = loader.MeshSize(index3ds, "Torus");
-	d = loader.MeshSize(index3ds, "TorusKnot");
-	if (d > fMinDistance)
-		fMinDistance = d;
-	d = loader.MeshSize(index3ds, "Teapot");
-	if (d > fMinDistance)
-		fMinDistance = d;
-
-	fDistance = fDefDistance = 0.5f * (fMaxDistance + fMinDistance);
-
-	CoordinateFrame::Instance().Make(fMaxDistance);
 }
 
 bool BigHeadScreamers::Resize(unsigned int width, unsigned int height)
@@ -217,8 +155,19 @@ bool BigHeadScreamers::Resize(unsigned int width, unsigned int height)
 	//printf("Reshaping Screen to %dx%d\n", width, height);
 	// Select and setup the projection matrix
 	glMatrixMode( GL_PROJECTION );
-	glLoadIdentity();
-	gluPerspective( 65.0f, (GLfloat)width/(GLfloat)height, Near, Far);
+	float fov = 65.0f * M_PI / 180.0f;
+	float aspect = (GLfloat)width/(GLfloat)height;
+	Matrix4 proj = ProjectionRH(fov, aspect, Near, Far);
+	//Matrix4 proj = ProjectionRHInfinite(fov, aspect, Near);
+	glLoadTransposeMatrixf(proj.data());
+	// Calculate inverse (used to draw infinite plane)
+	mInvProj = InverseProjectionRHInfinite(fov, 1.0/aspect, Near);
+
+	//glLoadIdentity();
+	//gluPerspective( 65.0f, (GLfloat)width/(GLfloat)height, Near, Far);
+
+	//float p[16];
+	//glGetFloatv(GL_PROJECTION_MATRIX, p);
 	return true;
 }
 
@@ -233,78 +182,12 @@ bool BigHeadScreamers::Render()
 	fpsGraph.Input(1.0f / dt, timer.GetTime());
 
 	/* User input */
-	if (KeyPressed(KEY_9))
-	{
-		bLightMove = !bLightMove;
-	}
-	if (KeyPressed(KEY_0))
-	{
-		bWireframe = !bWireframe;
-	}
-	if (KeyPressing(KEY_UP))
-	{
-		if ((fDistance -= timer.GetDeltaTime() * 150.0f) < fMinDistance)
-			fDistance = fMinDistance;
-	}
-	if (KeyPressing(KEY_DOWN))
-	{
-		if ((fDistance += timer.GetDeltaTime() * 150.0f) > fMaxDistance)
-			fDistance = fMaxDistance;
-	}
-	if (ScrollUp())
-	{
-		if ((fDistance /= 1.05f) < fMinDistance)
-			fDistance = fMinDistance;
-	}
-	if (ScrollDown())
-	{
-		if ((fDistance *= 1.05f) > fMaxDistance)
-			fDistance = fMaxDistance;
-	}
-
-	if (KeyPressing(KEY_Q))
-	{
-		light[iCurLight].fLightHeight += 50.0f * timer.GetDeltaTime();
-	}
-	if (KeyPressing(KEY_E))
-	{
-		light[iCurLight].fLightHeight -= 50.0f * timer.GetDeltaTime();
-	}
-	if (KeyPressing(KEY_W))
-	{
-		light[iCurLight].fLightRadius += timer.GetDeltaTime();
-	}
-	if (KeyPressing(KEY_S))
-	{
-		light[iCurLight].fLightRadius -= timer.GetDeltaTime();
-	}
-	if (KeyPressed(KEY_A))
-	{
-		if (iNumLights > 1)
-			iNumLights--;
-	}
-	if (KeyPressed(KEY_D))
-	{
-		if (iNumLights < MAX_LIGHTS)
-			iNumLights++;
-	}
 	launcher.Update(dt);
 	if (LeftClick() || KeyPressing(KEY_SPACE))
 	{
 		launcher.Fire(fpsCamera);
 	}
 
-	
-	// Update light positions
-	if (bLightMove)
-	{
-		for (unsigned int i = 0; i < MAX_LIGHTS; i++)
-		{
-			light[i].fLightAngle += timer.GetDeltaTime();
-
-			light[i].UpdatePos(fMinDistance);
-		}
-	}
 
 	/* Rendering */
 	// Clear color and depth buffer
@@ -323,21 +206,30 @@ bool BigHeadScreamers::Render()
 	SkyBox::Instance().Render(alpinCubeMap, fpsCamera.GetAlpha(),
 		fpsCamera.GetBeta(), Far / 10.0f);
 
-
 	fpsCamera.Update(this, timer.GetDeltaTime());
-	fpsCamera.LoadMatrix();
+	fpsCamera.LoadMatrixNoXZ();
+
 
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, uiBGTexture);
 
-	/*glEnable(GL_CULL_FACE);
 
-	RenderRoom(uiProgram[E_DIFFUSE]);
+	// Use this to query matrices and check the inverse is calculated
+	//correctly
+	/*float p[16], m[16];
+	glGetFloatv(GL_PROJECTION_MATRIX, p);
+	glGetFloatv(GL_MODELVIEW_MATRIX, m);
+	Matrix4 P = Matrix4(p).Transpose();
+	Matrix4 M = Matrix4(m).Transpose();*/
 
-	glDisable(GL_CULL_FACE);*/
+	Matrix4 rotation = AlphaBetaRotation(fpsCamera.GetAlpha(), fpsCamera.GetBeta());
+	//Vector3 translation = fpsCamera.GetTranslation();
+	Vector3 translationNoXZ = Vector3(0.0f, fpsCamera.GetTranslation()[1], 0.0f);
+	Matrix4 projViewInv = InverseMVP(mInvProj, translationNoXZ, rotation);
 
-	RenderScene();
+	ground.Render(projViewInv, -fpsCamera.GetTranslation(), Far);
 
+	fpsCamera.LoadMatrix();
 	RenderGrenades();
 
 	// Render Light
@@ -389,16 +281,16 @@ bool BigHeadScreamers::Render()
 			ttFont.SDL_GL_RenderText(color, &position, "beta=%.1f", fpsCamera.GetBeta());
 			position.y -= position.h * 1.2f;
 
-			const Vector3 tr = fpsCamera.GetTranslation();
-			ttFont.SDL_GL_RenderText(color, &position, "t=[%.1f,%.1f,%.1f]", tr[0], tr[1], tr[2]);
+			const Vector3 tr = -fpsCamera.GetTranslation();
+			ttFont.SDL_GL_RenderText(color, &position, "pos=[%.1f,%.1f,%.1f]", tr[0], tr[1], tr[2]);
 			position.y -= position.h * 1.2f;
 
-			ttFont.SDL_GL_RenderText(color, &position, "#=%d", uiInfPlaneVertices);
+			ttFont.SDL_GL_RenderText(color, &position, "#=%d", ground.VisibleVertices());
 			position.y -= position.h * 1.2f;
 
-			for (int i = 0; i < uiInfPlaneVertices; i++)
+			for (int i = 0; i < ground.VisibleVertices(); i++)
 			{
-				ttFont.SDL_GL_RenderText(color, &position, "w[%d]=[%.1f,%.1f,%.1f]", i, vGround[i][0], vGround[i][1], vGround[i][2]);
+				ttFont.SDL_GL_RenderText(color, &position, "w[%d]=[%.1f,%.1f,%.1f]", i, ground[i][0], ground[i][1], ground[i][2]);
 				position.y -= position.h * 1.2f;
 			}
 		}
@@ -441,153 +333,6 @@ void BigHeadScreamers::RenderGrenades()
 	glDisableClientState(GL_VERTEX_ARRAY);
 }
 
-void BigHeadScreamers::RenderRoom(GLuint program)
-{
-	/* Draw mesh */
-	glUseProgram(program);
-
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glEnableClientState(GL_NORMAL_ARRAY);
-	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-
-	glCullFace(GL_BACK);
-
-	if (program == uiProgram[E_DIFFUSE])
-	{
-		glUniform1i(GetUniLoc(program, "NumLights"), iNumLights);
-		glUniform4fv(GetUniLoc(program, "Color"), 1, GrayColor);
-	}
-	PrintOpenGLError();
-
-	pRoomMesh->GetVBO()->Render(GL_TRIANGLES);
-
-	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-	glDisableClientState(GL_NORMAL_ARRAY);
-	glDisableClientState(GL_VERTEX_ARRAY);
-}
-
-#define DRAW_PLANE4(X, T, MODE) \
-{\
-	float temp[] = {\
-		X[0][0], X[0][1], X[0][2], X[0][3], T, T, \
-		X[1][0], X[1][1], X[1][2], X[1][3], 0.0f, T,\
-		X[2][0], X[2][1], X[2][2], X[2][3], 0.0f, 0.0f,\
-		X[3][0], X[3][1], X[3][2], X[3][3], T, 0.0f\
-	};\
-	glVertexPointer(4, GL_FLOAT, sizeof(float) * 6, temp);\
-	glTexCoordPointer(2, GL_FLOAT, sizeof(float) * 6, temp + 4);\
-	glDrawArrays(MODE, 0, 4);\
-}
-
-#define DRAW_INFINITE_PLANE(X, T, N, MODE) \
-{\
-	float temp[] = {\
-		X[0][0], X[0][1], X[0][2], T, T, \
-		X[1][0], X[1][1], X[1][2], 0.0f, T,\
-		X[2][0], X[2][1], X[2][2], 0.0f, 0.0f,\
-		X[3][0], X[3][1], X[3][2], T, 0.0f\
-	};\
-	glVertexPointer(3, GL_FLOAT, sizeof(float) * 5, temp);\
-	glTexCoordPointer(2, GL_FLOAT, sizeof(float) * 5, temp + 3);\
-	glDrawArrays(MODE, 0, N);\
-}
-
-void BigHeadScreamers::RenderScene()
-{
-	float proj[16], mview[16];
-
-	Vector4 v[4];
-
-	float t = 100.0f;
-	float w = 0.0001f;
-	v[0] = Vector4(1.0f, 0.0f, 1.0f, w);
-	v[1] = Vector4(-1.0f, 0.0f, 1.0f, w);
-	v[2] = Vector4(-1.0f, 0.0f, -1.0f, w);
-	v[3] = Vector4(1.0f, 0.0f, -1.0f, w);
-
-	//glMatrixMode( GL_PROJECTION );
-	glGetFloatv(GL_PROJECTION_MATRIX, proj);
-	//glMatrixMode( GL_MODELVIEW );
-	glGetFloatv(GL_MODELVIEW_MATRIX, mview);
-
-	/*DebugMatrix4 mp(proj);
-	DebugMatrix4 mv(mview);
-
-	Matrix4 ip = mp.Inverse(0.1);
-
-	Matrix4 ii = ip * mp;*/
-
-	Matrix4 invProjView = (Matrix4(proj) * Matrix4(mview)).Inverse(0.1);
-
-	//Matrix4 ii = invProjView * Matrix4(proj) * Matrix4(mview);
-
-
-	Vector3 eyePos = -fpsCamera.GetTranslation();
-	const float plane[] = { 0.0f, 1.0f, 0.0f, 0.0f };
-	uiInfPlaneVertices = InfinitePlane(vGround, plane, invProjView, eyePos, Far);
-
-
-	Vector4 u[4];
-	Vector4 x[4];
-	for (unsigned int i = 0; i < 4; i++)
-	{
-		Projection(u[i], proj, mview, v[i]);
-		ZeroZ(x[i], u[i]);
-	}
-
-	if (KeyPressed(KEY_1))
-	{
-		cout << "Modelview matrix\n";
-		DebugMatrix4::GLPrint(mview, "%5.2f");
-		cout << "Projection matrix\n";
-		DebugMatrix4::GLPrint(proj, "%5.2f");
-
-		for (unsigned int i = 0; i < 4; i++)
-		{
-			printf("Projection of [%.1f, %.1f, %.1f, %.1f] is "\
-				"[%.1f, %.1f, %.1f, %.1f]\n",
-				v[i][0], v[i][1], v[i][2], v[i][3],
-				u[i][0], u[i][1], u[i][2], u[i][3]);
-		}
-	}
-
-	glEnableClientState(GL_VERTEX_ARRAY);	
-	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-
-	/*glUseProgram(uiProgram[E_INFINITY]);
-
-	float repeat[] = { 0.01f, 0.01f };
-	glUniform2fv(GetUniLoc(uiProgram[E_INFINITY], "TexRepeat"), 1, repeat);
-	glUniform2fv(GetUniLoc(uiProgram[E_INFINITY], "TexOffset"), 1, &eyePos[0]);
-
-	DRAW_INFINITE_PLANE(vGround, t, uiInfPlaneVertices, GL_TRIANGLE_FAN)*/
-
-	glUseProgram(uiProgram[E_LOOKUP]);
-	DRAW_PLANE4(v, t, GL_TRIANGLE_FAN)
-
-	glUseProgram(uiProgram[E_IDENTITY]);
-	float color[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	glUniform4fv(GetUniLoc(uiProgram[E_IDENTITY], "Color"), 1, color);
-
-	//DRAW_PLANE4(u, t, GL_LINE_LOOP)
-	// X has zeroed z component so it's unaffected by depth test
-	DRAW_PLANE4(x, t, GL_LINE_LOOP)
-
-	glDisableClientState(GL_VERTEX_ARRAY);	
-	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-}
-
-void BigHeadScreamers::Projection(Vector4 &u, const float *proj, const float *mview, const Vector4 &v)
-{
-	u = DebugMatrix4::GLMult(mview, v);
-	u = DebugMatrix4::GLMult(proj, u);
-}
-
-void BigHeadScreamers::ZeroZ(Vector4 &w, const Vector4 &u)
-{
-	w = Vector4(u[0], u[1], 0.0f, u[3]);
-}
-
 
 void BigHeadScreamers::DrawLightMarker(float *lightPos)
 {
@@ -608,10 +353,10 @@ void BigHeadScreamers::DrawCoordinateFrame()
 	float offset[] = { -0.9f, -0.9f };
 	glUniform2fv(GetUniLoc(uiProgram[E_COLOR_OFFSET], "Offset"), 1, offset);
 
-	//fpsCamera.LoadMatrix();
 	glLoadIdentity();
 
-	glTranslatef(0.0f, 0.0f, -fDefDistance);
+	glTranslatef(0.0f, 0.0f, -271.92487);
+	//glTranslatef(0.0f, 0.0f, -fDefDistance);
 	glRotatef(fpsCamera.GetBeta(), 1.0f, 0.0f, 0.0f);
 	glRotatef(fpsCamera.GetAlpha(), 0.0f, 1.0f, 0.0f);
 
@@ -624,7 +369,7 @@ bool BigHeadScreamers::ReleaseGL()
 	CoordinateFrame::Instance().Unload();
 
 	delete pTetraVBO;
-	delete pRoomMesh;
+	//delete pRoomMesh;
 
 	return GLResourceManager::Instance().Release();
 }
--- demos/big-head-screamers/BigHeadScreamers.h
+++ demos/big-head-screamers/BigHeadScreamers.h
@@ -7,13 +7,14 @@
  *
  * Platform			LinuxX11 / OpenGL
  * 
- * Description		Lighting & BigHeadScreamers demo using stencil buffers
+ * Description		BigHeadScreamers
  *
  *****************************************************************************/
 
 #ifndef _BIG_HEAD_SCREAMERS_
 #define _BIG_HEAD_SCREAMERS_
 
+#include "version.h"
 
 #include "Extensions.h"
 #include "SDLShell.h"
@@ -31,147 +32,54 @@
 #include "CoordinateFrame.h"
 
 #include "BaseGraph.h"
-#include "Mesh.h"
-#include "ShadowVolume.h"
-
-#include "version.h"
-
 
 #include "Matrix.h"
 #include "CameraController.h"
 #include "Weapon.h"
-
 #include "SkyBox.h"
-
-
-#define GROUPS_COUNT 3
-#define MAX_LIGHTS 4
-
-enum Shaders {
-	E_LOOKUP, 
-	E_LOOKUP_COLOR, 
-	E_INFINITY, 
-	E_IDENTITY, 
-	E_DIFFUSE, 
-	E_COLOR_OFFSET,
-	E_UNIFORM,
-	E_NUM_PROGRAMS 
-};
-
-const char *shaders[] = {
-	"data/shaders/Lookup.vert", "data/shaders/Lookup.frag",
-	"data/shaders/LookupColor.vert", "data/shaders/LookupColor.frag",
-	"data/shaders/Infinite.vert", "data/shaders/Infinite.frag",
-	"data/shaders/Identity.vert", "data/shaders/Identity.frag",
-	"data/shaders/DiffuseColor.vert", "data/shaders/DiffuseColor.frag",
-	"data/shaders/ColorOffset.vert", "data/shaders/ColorOffset.frag",
-	"data/shaders/UniformColor.vert", "data/shaders/UniformColor.frag",
-};
-
-
-enum Primitives {
-	MESH_TORUS_KNOT, 
-	MESH_TORUS, 
-	MESH_SPHERE,
-	MESH_CUBE,
-	MESH_MONKEY, 
-	MESH_NUM 
-};
-
-const char *meshes[] = {
-	"TorusKnot",
-	"Torus",
-	"Sphere",
-	"Cube",
-	"Suzanne",
-};
-
-
-
+#include "Ground.h"
 
 class BigHeadScreamers : public SDLShell
 {
+	enum Shaders {
+		E_LOOKUP, 
+		E_LOOKUP_COLOR, 
+		E_COLOR_OFFSET,
+		E_UNIFORM,
+		E_NUM_PROGRAMS 
+	};
+
 	Pointer *NewPointer() { return new FPSPointer(this); }
 protected:
-	GLuint uiProgram[E_NUM_PROGRAMS];
+	// Game Data
+	GrenadeLauncher launcher;
 
-	CubeMap alpinCubeMap;
-
+	// POD part of the file
 	int iShowInfo;
+	GLuint uiBGTexture;
 
-	bool bWireframe;
+	IndexedVBO *pTetraVBO;
+
+	// Shader program files
+	GLuint uiProgram[E_NUM_PROGRAMS];
 
 	Timer timer;
 
 	TTFont ttFont;
 
-	GLuint uiBGTexture;
-	GLuint uiCoordFrameList;
-
-	float fMaxDistance, fMinDistance;
-	float fDefDistance;
-
-	float fBoxSize;
-	float fDistance;
-
-	// Temp
-	unsigned int uiInfPlaneVertices;
-	Vector3 vGround[5];
-
-
-	// Light defines a light structure containing all variables used for
-	// animation, and the position to be passed to the API
-	struct Light
-	{
-		float fLightSpeed;
-		float fLightAngle;
-		float fLightHeight;
-		float fLightRadius;
-		//! Position to be passed to glLightfv
-		float fPos[4];
-
-		void UpdatePos(float minDistance)
-		{
-			fPos[0] = fLightRadius * minDistance * 
-				cos(fLightAngle * fLightSpeed);
-			fPos[1] = fLightHeight;
-			fPos[2] = fLightRadius * minDistance *
-				sin(fLightAngle * fLightSpeed);
-			fPos[3] = 1.0f;
-		}
-
-	} light[MAX_LIGHTS];
-	unsigned int iCurLight;
-	unsigned int iNumLights;
-
-	bool bLightMove;
-
-
-	IndexedVBO *pTetraVBO;
-	Mesh *pRoomMesh;
+	CubeMap alpinCubeMap;
+	// Inverted projection matrix (needed by infinite plane rendering)
+	Matrix4 mInvProj;
+	Ground ground;
 
 	BaseGraph fpsGraph;
-
 	FPSCamera fpsCamera;
 
+protected:
 	bool LoadShaders();
 
 	void DrawLightMarker(float *lightPos);
 	void DrawCoordinateFrame();
-	void CalculateBoundaries(int index3ds);
-
-	void RenderRoom(GLuint program);
-	void RenderScene();
-
-	void RenderPlane(GLuint program, const float *color);
-
-	GLenum GLRenderMode() { return bWireframe ? GL_LINES : GL_TRIANGLES; }
-
-	void Projection(Vector4 &u, const float *proj, const float *mview, const Vector4 &v);
-	void ZeroZ(Vector4 &w, const Vector4 &u);
-
-	// Game Data
-	GrenadeLauncher launcher;
 
 	void RenderGrenades();
 
