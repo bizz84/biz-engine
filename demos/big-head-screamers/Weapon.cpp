b990315728d16573a95abe5cbd687d1bfcd2c48e
Andrea Bizzotto <bizz84@gmail.com>
2010-07-20 18:49:53 +0100

    - Rearranged to class hierarchy

--- demos/big-head-screamers/Weapon.cpp
+++ demos/big-head-screamers/Weapon.cpp
@@ -18,76 +18,125 @@
 #define M_RAD (180.0 / M_PI)
 #define M_1_RAD (M_PI / 180.0)
 
+/*****************************************************************************
+ * Bullet implementation
+ *****************************************************************************/
+
+ Bullet::Bullet(const Point3 &p, const float yRot, const float xRot,
+		const float speed)
+{
+	hit = false;
+	pos[1] = -p;
+	pos[1][1] -= 10.0f;
+	pos[0] = pos[1];
+	vel = Matrix3::RotationY(-yRot) * (Matrix3::RotationX(-xRot) *
+		Vector3(0.0f, 0.0f, -speed));		
+}
+		
+/*****************************************************************************
+ * Grenade implementation
+ *****************************************************************************/
+ 
 const float Grenade::g = 2.0f;
 
 Grenade::Grenade(const Point3 &p, const float yRot, const float xRot,
-				 const float speed)
+				 const float speed) : Bullet(p, yRot, xRot, speed)
 {
 	bounces = 0;
-	pos = -p;
-	pos[1] -= 10.0f;
-	vel = Matrix3::RotationY(-yRot) * (Matrix3::RotationX(-xRot) *
-		Vector3(0.0f, 0.0f, -speed));
 }
 
 
 bool Grenade::Update(float dt)
 {
+	pos[0] = pos[1];
 	dt *= 20.0f;
 	float f = g * dt;
 
 	vel[1] -= f;
 
-	pos += vel * dt;
+	pos[1] += vel * dt;
 
-	if (pos[1] < 0.0f)
+	if (pos[1][1] < 0.0f)
 	{
 		bounces++;
-		pos[1] = -pos[1];
+		pos[1][1] = -pos[1][1];
 		vel[0] *= 0.5f;
 		vel[1] *= -0.5f;
 		vel[2] *= 0.5f;
 	}
+	if (bounces >= MAX_BOUNCES)
+		hit = true;
 	
 	return !Impact();
 
 }
 
-const bool Grenade::Impact() const
+/*****************************************************************************
+ * WeaponSystem implementation
+ *****************************************************************************/
+WeaponSystem::~WeaponSystem()
 {
-	return bounces >= MAX_BOUNCES;/*pos.s[1] <= 0.0f;*/
+	list<Bullet *>::iterator iter;
+	for (iter = bullets.begin(); iter != bullets.end(); iter++)
+	{
+		delete *iter;
+	}
 }
 
+Bullet *WeaponSystem::NewBullet(const Point3 &p, const float yRot,
+								const float xRot, const float speed)
+{
+	//if (eCurrWeapon == Grenade)
+	return new Grenade(p, yRot, xRot, speed);
+}
 
-bool GrenadeLauncher::Fire(const FPSCamera &cameraPos)
+bool WeaponSystem::Fire(const FPSCamera &cameraPos)
 {
 	if (canFire)
 	{
 		canFire = false;
 		time = 0.0f;
-		Grenade grenade(cameraPos.GetTranslation(),
-			cameraPos.GetAlpha() * M_1_RAD, cameraPos.GetBeta() * M_1_RAD, 50.0f);
-		grenades.push_back(grenade);
+		// add
+		bullets.push_back(NewBullet(cameraPos.GetTranslation(),
+			cameraPos.GetAlpha() * M_1_RAD, cameraPos.GetBeta() * M_1_RAD, 50.0f));
 		return true;
 	}
 	return false;
 }
 
-bool Condition(const Grenade &grenade)
+bool ImpactCondition(const Bullet *bullet)
 {
-	return grenade.Impact();
+	return bullet->Impact();
 }
 
-void GrenadeLauncher::Update(const float dt)
+bool ExplosionCondition(const BulletExplosion *explosion)
+{
+	return explosion->Ended();
+}
+
+void WeaponSystem::Update(const float dt)
 {
 	if ((time += dt) >= reloadTime && canFire == false)
 	{
 		canFire = true;
 	}
-	list<Grenade>::iterator iter;
-	for (iter = grenades.begin(); iter != grenades.end(); iter++)
+	// Update grenades
+	// TODO: should be a generic Bullet * routine
+	list<Bullet *>::iterator b;
+	for (b = bullets.begin(); b != bullets.end(); b++)
 	{
-		iter->Update(dt);
+		(*b)->Update(dt);
 	}
-	grenades.remove_if(Condition);
+	// TODO: perform this after collision detection
+	bullets.remove_if(ImpactCondition);
+	
+	// Update explosions
+	// TODO: should be a generic Explosion * routine
+	list<BulletExplosion *>::iterator e;	
+	for (e = explosions.begin(); e != explosions.end(); e++)
+	{
+		(*e)->Update(dt);
+	}
+	explosions.remove_if(ExplosionCondition);	
+	
 }
--- demos/big-head-screamers/Weapon.h
+++ demos/big-head-screamers/Weapon.h
@@ -22,38 +22,120 @@
 
 using namespace std;
 
-class Grenade
+/*****************************************************************************
+ * Bullet class declaration
+ *****************************************************************************/
+ 
+class Bullet
+{
+protected:
+	// store previous and current (needed for collision detection)
+	Point3 pos[2];
+	Vector3 vel;
+	bool hit;
+public:
+	Bullet(const Point3 &p, const float yRot, const float xRot,
+		const float speed);
+	virtual ~Bullet() { }
+		
+	virtual bool Update(const float dt) = 0;
+
+	const bool Impact() const { return hit; }
+
+	const Point3 GetPosition() const { return pos[1]; }
+	const Point3 GetPrevPosition() const { return pos[0]; }
+	
+	virtual const unsigned int Damage() const = 0;
+};
+
+/*****************************************************************************
+ * Grenade class declaration
+ *****************************************************************************/
+
+class Grenade : public Bullet
 {
 	unsigned int bounces;
-	Point3 pos;
-	Vector3 vel;
 
 	static const float g;
 public:
 	Grenade(const Point3 &p, const float yRot, const float xRot,
 		const float speed);
-	bool Update(float dt);
+	virtual bool Update(const float dt);
 
-	const bool Impact() const;
-
-	const Point3 GetPosition() const { return pos; }
 public:
 	enum { MAX_BOUNCES = 3 };
+	enum { DAMAGE = 50 };
+	virtual const unsigned int Damage() const { return DAMAGE; }
 };
 
-class GrenadeLauncher
+/*****************************************************************************
+ * BulletExplosion class declaration
+ *****************************************************************************/
+ 
+class BulletExplosion
 {
+	Point3 pos;
+	float time;
+public:
+	BulletExplosion(const Point3 &p) : pos(p), time(0.0f) { }
+	virtual ~BulletExplosion() { }
+	
+	bool Update(const float dt) { return (time += dt) < LifeTime(); }
+	
+	virtual const bool Ended() const { return time >= LifeTime(); }
+	
+	virtual const float LifeTime() const = 0;
+};
+
+/*****************************************************************************
+ * GrenadeExplosion class declaration
+ *****************************************************************************/
+
+class GrenadeExplosion : public BulletExplosion
+{
+
+public:
+	GrenadeExplosion(const Point3 &p) : BulletExplosion(p) { }
+	
+	virtual const float LifeTime() const { return 3.0f; }
+};
+
+/*****************************************************************************
+ * WeaponSystem class declaration
+ *****************************************************************************/
+
+class WeaponSystem
+{
+public:
+	enum WeaponType { TypeGrenade, TypePlasma, NumWeapons };
+private:
+
 	const float reloadTime;
 	float time;
+	// TODO: firing time depends on the weapon used
 	bool canFire;
-	list<Grenade> grenades;
+
+	WeaponType eCurrWeapon;
+
+	// TODO: this should be a list of <Bullet *>
+	list<Bullet *> bullets;
+	// TODO: this should be a list of <Explosion *>
+	list<BulletExplosion *> explosions;
 public:
-	GrenadeLauncher(const float reloadTime)
-		: reloadTime(reloadTime), time(0.0f), canFire(true) { }
+	WeaponSystem(const float reloadTime)
+		: reloadTime(reloadTime), time(0.0f), canFire(true), eCurrWeapon(TypeGrenade) { }
+	~WeaponSystem();
+
+	Bullet *NewBullet(const Point3 &p, const float yRot, const float xRot,
+		const float speed);
+
+	// handles fire request
 	bool Fire(const FPSCamera &cameraPos);
 	void Update(const float dt);
 
-	const list<Grenade> &GetGrenades() const { return grenades; }
+	const list<Bullet *> &GetBullets() const { return bullets; }
+	
+	void AddExplosion(const Point3 &p) { explosions.push_back(new GrenadeExplosion(p)); }
 };
 
 #endif
