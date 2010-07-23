199a14dc98bf7e02c8531d4f62a108932e5a9e73
Andrea Bizzotto <bizz84@gmail.com>
2010-07-23 22:08:13 +0100

    - Added EnemyRenderer interface

--- demos/big-head-screamers/Enemy.h
+++ demos/big-head-screamers/Enemy.h
@@ -14,6 +14,13 @@
 #define _ENEMY_H_
 
 #include "Vector.h"
+#include <vector>
+
+using namespace std;
+
+/*****************************************************************************
+ * class defining an enemy
+ *****************************************************************************/
 
 class Enemy
 {
@@ -24,16 +31,16 @@ protected:
 public:
 	virtual ~Enemy() { }
 
-
-	//const Vector2 &GetPos() const { return pos; }
-	//const int Health() const { return health; }
-
 	// public
 	Vector2 pos;
 	int health;
 
 	bool Dead() const { return health <= 0; }
 };
+
+/*****************************************************************************
+ * Sprite-type enemy (adds the texIndex depending on health)
+ *****************************************************************************/
 
 class SpriteEnemy : public Enemy
 {
@@ -45,6 +52,25 @@ public:
 	int texIndex0, texIndex1;
 
 	int GetTextureIndex() { return health <= 50 ? texIndex1 : texIndex0; }
+};
+
+
+/*****************************************************************************
+ * Base EnemyRenderer class for rendering billboarded enemies
+ *****************************************************************************/
+
+class EnemyRenderer
+{
+public:
+	// This needs to be accessible elsewhere
+	enum { NUM_SPRITES = 8 };
+public:
+	virtual ~EnemyRenderer() { }
+	virtual bool LoadSprites() = 0;
+	virtual bool Update(const vector<Enemy *> &data, const float angle,
+		const float height) { return false; }
+	virtual void Render(const vector<Enemy *> &data, const float angle,
+		const float height) const = 0;
 };
 
 #endif
