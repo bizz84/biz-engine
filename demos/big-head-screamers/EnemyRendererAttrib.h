41f370b02ba4f96788181290145a9d7d404c9aea
Andrea Bizzotto <bizz84@gmail.com>
2010-07-23 22:06:55 +0100

    - Initial revision

---
+++ demos/big-head-screamers/EnemyRendererAttrib.h
@@ -0,0 +1,89 @@
+/*****************************************************************************
+ * Filename			Weapon.h
+ * 
+ * License			LGPL
+ *
+ * Author			Andrea Bizzotto (bizz84@gmail.com)
+ *
+ * Platform			LinuxX11 / OpenGL
+ * 
+ * Description		Weapon
+ *
+ *****************************************************************************/
+#ifndef _ENEMY_RENDERER_ATTRIB_H_
+#define _ENEMY_RENDERER_ATTRIB_H_
+
+#include "Extensions.h"
+#include "ProgramArray.h"
+#include "EnemyRenderer.h"
+
+#include <vector>
+using namespace std;
+
+
+
+/*****************************************************************************
+ * Derived class (one draw call for all enemies via instancing)
+ *****************************************************************************/
+
+// is-implemented-in-terms-of
+class EnemyRendererAttrib : public EnemyRenderer, private ProgramArray
+{
+public:
+private:
+	enum EProgram {	P_SPRITE_ATTRIB, NUM_PROGRAMS };
+
+	// Attributes used by the instancing shader
+	enum {
+		A_VERTEX,
+		A_TEX_COORD,
+		A_SCALE,
+		A_ROT_ANGLE,
+		A_TRANSLATE,
+		P_ATTRIBS
+	};
+	GLint attribLoc[P_ATTRIBS];
+
+	GLuint UseProgram(int index) const;
+
+	GLuint uiAtlas;
+
+	struct SpriteVertexData
+	{
+		Point2 pos;
+		Point2 tex;
+		
+		float scale;
+		float rotAngle;
+		Vector3 translation;
+		
+		void SetAttributes(float s, float rot, const Vector3 &t)
+		{
+			scale = s;
+			rotAngle = rot;
+			translation = t;
+		}
+	};
+	SpriteVertexData *attrib;
+
+	bool LoadSprites();
+
+	static Point2 TexCoord(Point2 coord, int index);
+
+	bool SetAttribPointer(GLint loc, size_t size, GLenum type, void *address) const;
+	bool UnsetAttribPointer(GLint loc) const;
+
+public:
+	EnemyRendererAttrib();
+	~EnemyRendererAttrib();
+
+	virtual bool Update(const vector<Enemy *> &data, const float angle,
+		const float height);
+
+	virtual void Render(const vector<Enemy *> &data, const float angle,
+		const float height) const;	
+
+	static const unsigned int NumSprites;
+};
+
+#endif
