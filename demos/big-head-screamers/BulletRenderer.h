aa50ec60d17117cea49989443c51ef7fa4e1cea3
Andrea Bizzotto <bizz84@gmail.com>
2010-07-24 19:04:32 +0100

    - Initial revision

---
+++ demos/big-head-screamers/BulletRenderer.h
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * Filename			Weapon.h
+ * 
+ * License			LGPL
+ *
+ * Author			Andrea Bizzotto (bizz84@gmail.com)
+ *
+ * Platform			LinuxX11 / OpenGL
+ * 
+ * Description		Weapon
+ *
+ *****************************************************************************/
+#ifndef _BULLET_RENDERER_H_
+#define _BULLET_RENDERER_H_
+
+#include "Extensions.h"
+
+#include <list>
+using namespace std;
+
+class Bullet;
+
+class BulletRenderer
+{
+public:
+	virtual ~BulletRenderer() { }
+	virtual void Render(const list<Bullet *> &bullets) const = 0;
+};
+
+#endif
