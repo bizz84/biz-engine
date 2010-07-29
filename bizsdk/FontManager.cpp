ef69bf56bb300be2758bc216fc11ee7870319786
Andrea Bizzotto <bizz84@gmail.com>
2010-07-29 21:55:52 +0100

    - Initial revision

---
+++ bizsdk/FontManager.cpp
@@ -0,0 +1,172 @@
+/*****************************************************************************
+ * Filename			FontManager.cpp
+ * 
+ * License			LGPL
+ *
+ * Author			Andrea Bizzotto (bizz84@gmail.com)
+ *
+ * Platform			LinuxX11 / OpenGL
+ * 
+ * Description		Font Manager
+ *
+ *****************************************************************************/
+
+#include "FontManager.h"
+#include "GLResourceManager.h"
+
+FontManager::FontManager(const char *file, bool fixed)
+	: textureFile(file), fixedWidth(fixed)
+{
+	for (unsigned int i = 0; i < NUM_GLYPHS; i++)
+	{
+		aiGlyphLookup[i] = i; // empty glyph
+	}
+
+}
+
+bool FontManager::LoadShaders()
+{
+	const char vertexShader[] = \
+		"varying vec2 TexCoord;" \
+		"void main()" \
+		"{" \
+			"gl_Position = vec4(gl_Vertex.x, gl_Vertex.y, 0.0, 1.0);" \
+			"TexCoord = gl_MultiTexCoord0.xy;" \
+		"}";
+
+	const char fragmentShader[] = \
+		"uniform sampler2D sTexture;" \
+		"uniform vec4 Color;" \
+		"varying vec2 TexCoord;" \
+		"void main()" \
+		"{" \
+			"gl_FragColor = texture2D(sTexture, TexCoord);" \
+			"gl_FragColor.a = gl_FragColor.r * 4.0 * Color.a;" \
+			"gl_FragColor.xyz *= Color.xyz;" \
+		"}";
+		
+	GLResourceManager &loader = GLResourceManager::Instance();
+	if (!loader.LoadShaderFromMemory(vertexShader, fragmentShader, program))
+		return false;
+	
+	glUseProgram(program);
+	glUniform1i(GetUniLoc(program, "sTexture"), 0);
+	locColor = GetUniLoc(program, "Color");
+
+	return true;
+}
+
+bool FontManager::LoadTexture()
+{
+	GLResourceManager &loader = GLResourceManager::Instance();
+	if (!loader.LoadTextureFromFile(textureFile.c_str(), texture, GL_LINEAR, GL_LINEAR))
+		return false;
+
+	return true;
+}
+
+bool FontManager::Init()
+{
+	if (!LoadShaders())
+		return false;
+
+	if (!LoadTexture())
+		return false;
+
+	GenerateGlyphs();
+	return true;
+}
+
+
+void FontManager::Bind() const
+{
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glUseProgram(program);	
+}
+
+const float FontManager::Width(const char *str, float height) const
+{
+	int len = strlen(str);
+	if (FixedWidth())
+		return len * height;
+
+	float width = 0.0f;
+
+	unsigned int i;
+	for (i = 0; i < len; i++)
+	{
+		unsigned char c = str[i];
+		const Glyph &g = glyph[GlyphIndex(str[i])];
+		width += g.Ratio() * height;
+	}
+	return width;
+}
+
+void FontManager::Render(float posX, float posY, float height, float *color,
+	HorzAlign halign, VertAlign valign, const char *text, ...) const
+{
+	// format string
+	char str[1024];
+	va_list va_args;
+    va_start( va_args, text );
+    int retval = _vsnprintf( str, 1024, text, va_args );
+    va_end(va_args);
+
+	// Adjust to aspect ratio
+	float width = Width(str, height);
+	float startX = posX;
+	float startY = posY;
+	if (halign == CenterAlign)
+		startX -= 0.5f * width;
+	else if (halign == RightAlign)
+		startX -= width;
+	
+	if (valign == MiddleAlign)
+		startY += 0.5f * height;
+	else if (valign == BottomAlign)
+		startY += height;
+
+	// tex coord multiplier
+	float h = height;
+		
+	// each vertex = [x,y,u,v], 4 vertices per glyph
+	int len = strlen(str);
+	float *attribs = new float[4 * 4 * len];
+	float *a = attribs;	
+	
+	unsigned int i;
+	for (i = 0; i < len; i++)
+	{
+		int index = GlyphIndex(str[i]);
+		const Glyph &g = glyph[index];
+		float w = g.Ratio() * height;
+		*a++ = startX;
+		*a++ = startY;
+		*a++ = g.x;
+		*a++ = g.y;
+
+		*a++ = startX + w;
+		*a++ = startY;
+		*a++ = g.x + g.w;
+		*a++ = g.y;
+
+		*a++ = startX + w;
+		*a++ = startY - h;
+		*a++ = g.x + g.w;
+		*a++ = g.y + g.h;
+
+		*a++ = startX;
+		*a++ = startY - h;
+		*a++ = g.x;
+		*a++ = g.y + g.h;
+		
+		startX += w;
+	}
+	
+	glUniform4fv(locColor, 1, color);
+
+	glVertexPointer(2, GL_FLOAT, sizeof(float) * 4, attribs);
+	glTexCoordPointer(2, GL_FLOAT, sizeof(float) * 4, attribs + 2);
+	glDrawArrays(GL_QUADS, 0, len * 4);
+}
+
---
+++ bizsdk/FontManager.h
@@ -0,0 +1,88 @@
+/*****************************************************************************
+ * Filename			FontManager.h
+ * 
+ * License			LGPL
+ *
+ * Author			Andrea Bizzotto (bizz84@gmail.com)
+ *
+ * Platform			LinuxX11 / OpenGL
+ * 
+ * Description		FontManager
+ *
+ *****************************************************************************/
+
+#ifndef _FONT_MANAGER_H_
+#define _FONT_MANAGER_H_
+
+#include "Extensions.h"
+
+#include <string>
+using namespace std;
+
+#include "boost/ptr_container/ptr_vector.hpp"
+using namespace boost;
+
+// Simple font manager class. Not particularly well designed, supports only
+// one Font type stored in a texture
+// No caching or other techiques are used to increase efficiency
+class FontManager
+{
+protected:
+	class Glyph
+	{
+	public:
+		// texture coordinates
+		float x, y, w, h;
+
+		Glyph() : x(0.0f), y(0.0f), w(0.0f), h(0.0f) { }
+		Glyph(float x, float y, float w, float h)
+			: x(x), y(y), w(w), h(h) { }
+		Glyph(int x0, int y0, int x1, int y1, int texW, int texH)
+			: x((float)x0 / (float)texW), y((float)y0 / (float)texH),
+			w((float)(x1 - x0) / (float)texW), h((float)(y1 - y0) / (float)texH) { }
+
+		const float Width() const { return w; }
+		const float Height() const { return h; }
+
+		const float Ratio() const { return h == 0.0 ? 1.0f : w / h; }
+	};
+	ptr_vector<Glyph> glyph;
+	
+	enum { NUM_GLYPHS = 128 };
+	int aiGlyphLookup[NUM_GLYPHS];
+
+	bool fixedWidth;
+
+	string textureFile;
+	
+	GLuint program;
+	GLuint texture;
+	GLint locColor;
+
+	virtual void GenerateGlyphs() = 0;
+	bool LoadShaders();
+	bool LoadTexture();
+public:
+	FontManager(const char *file, bool fixed);
+	virtual ~FontManager() { }
+
+	bool Init();
+	
+	void Bind() const;
+
+	const float Width(const char *str, float height) const;
+
+	enum HorzAlign { LeftAlign, CenterAlign, RightAlign };
+	enum VertAlign { TopAlign, MiddleAlign, BottomAlign };
+	//! Render: this function formats the input string via va_list, creates a
+	// a vertex buffer and passes it to GL for rendering
+	void Render(float posX, float posY, float height, float *color,
+		HorzAlign halign, VertAlign valign, const char *text, ...) const;
+
+	const int GlyphIndex(unsigned char ch) const { return aiGlyphLookup[ch]; }	
+
+	const bool FixedWidth() const { return fixedWidth; }
+};
+
+
+#endif
