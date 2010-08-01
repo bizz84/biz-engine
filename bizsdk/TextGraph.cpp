be9bc9896f9d4b7a2378920de44ab426855ad710
Andrea Bizzotto <bizz84@gmail.com>
2010-08-01 22:20:20 +0100

    - Added small offset

--- bizsdk/TextGraph.cpp
+++ bizsdk/TextGraph.cpp
@@ -14,11 +14,14 @@
 #include "TextGraph.h"
 #include "FontManager.h"
 
+static const float z = 0.0f;
+static const float u = 1.0f;
+static const float v = 1.0f;
 
-static const float green[] = { 0.0f, 1.0f, 0.0f, 1.0f };
-static const float red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-static const float white[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-static const float yellow[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+static const float green[] = { z, v, z, u };
+static const float red[] = { v, z, z, u };
+static const float white[] = { v, v, v, u };
+static const float yellow[] = { v, v, z, u };
 
 bool TextGraph::Init(bool showMax, bool showMin, bool showAvg, bool showCur,
 		const char *precision, float textScale, float timeFrame, unsigned int samples, 
@@ -42,23 +45,23 @@ void TextGraph::TextDraw(const FontManager *font) const
 {
 	if (bShowMax)
 	{
-		font->Render(fX1, fY1, fTextScale, (float *)green, FontManager::RightAlign,
+		font->Render(fX1, fY1 + 0.01f, fTextScale, (float *)green, FontManager::RightAlign,
 			FontManager::BottomAlign, szPrecision, fCurrentMax);
 	}
 
 	if (bShowMin)
 	{
-		font->Render(fX1, fY0, fTextScale, (float *)green, FontManager::RightAlign,
+		font->Render(fX1, fY0 - 0.01f, fTextScale, (float *)green, FontManager::RightAlign,
 			FontManager::TopAlign, szPrecision, fCurrentMin);
 	}
 	if (bShowAvg)
 	{
-		font->Render(fX0, fY0, fTextScale, (float *)yellow, FontManager::LeftAlign,
+		font->Render(fX0, fY0 - 0.01f, fTextScale, (float *)yellow, FontManager::LeftAlign,
 			FontManager::TopAlign, szPrecision, fCurrentSum / (float)uiCurSamples);
 	}
 	if (bShowCur)
 	{
-		font->Render(fX0, fY1, fTextScale, (float *)white, FontManager::LeftAlign,
+		font->Render(fX0, fY1 + 0.01f, fTextScale, (float *)white, FontManager::LeftAlign,
 			FontManager::BottomAlign, szPrecision, fCurValue);
 	}
 }
