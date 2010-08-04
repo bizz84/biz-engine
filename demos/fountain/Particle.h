d9c5c438bcd2d42ac82ef268cb6a204f2660cfda
Andrea Bizzotto <bizz84@gmail.com>
2010-08-04 17:44:29 +0100

    - Changed license

--- demos/fountain/Particle.h
+++ demos/fountain/Particle.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * Filename			NBody.h
  * 
- * License			LGPL
+ * License			GPLv3
  *
  * Author			Andrea Bizzotto (bizz84@gmail.com)
  *
