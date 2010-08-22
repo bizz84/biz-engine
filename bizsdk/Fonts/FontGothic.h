9d59702d42470574c93a5276d7a0ce3f47fe45d3
Andrea Bizzotto <bizz84@gmail.com>
2010-08-22 09:35:24 +0100

    - Initial Revision

---
+++ bizsdk/Fonts/FontGothic.h
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * Filename			FontManager.h
+ * 
+ * License			GPLv3
+ *
+ * Author			Andrea Bizzotto (bizz84@gmail.com)
+ *
+ * Platform			LinuxX11 / OpenGL
+ * 
+ * Description		FontManager
+ *
+ *****************************************************************************/
+
+#ifndef _FONT_GOTHIC_H_
+#define _FONT_GOTHIC_H_
+
+#include "FontManager.h"
+
+class FontGothic : public FontManager
+{
+protected:
+	virtual void GenerateGlyphs();
+public:
+	FontGothic(const char *file) : FontManager(file, false) { }
+};
+
+#endif
